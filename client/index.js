4676405f41895a31f546260febe025c26f8436ef
Philippe Oscar Sanoy <sanoyphilippe@gmail.com>
2016-10-09 11:47:51 +0800

    index page setup. no functionality yet.

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+import './index.html';
+
+Template.front.onRendered(function () {
+})
